848562e2ff6a006666ddce0bd52081b78e27eef9
WolfieIA <bella.nguyen3004@gmail.com>
2022-08-05 23:03:56 +0800

    moved jay's shit to final project

--- Final Project/Lan/field-combi.js
+++
@@ -1,5 +0,0 @@
-
-$( "#adults" ).change(function(){
-        var $this = $( this );
-        $this.find( "option:first" ).text($this.val() + ' adult(s)').val( $this.val() ).prop('selected',true);
-    } )
